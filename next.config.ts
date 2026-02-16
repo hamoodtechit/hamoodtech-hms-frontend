81823eb17e1c04330d55ef206b228033504b32e7
hamoodtechit <hamoodtechit@gmail.com>
2026-02-16 14:21:51 +0600

    Fix TS error in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const withNextIntl = createNextIntlPlugin(
 );
  
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: import('next').NextConfig = {
   output: "standalone",
 };
  
