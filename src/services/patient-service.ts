680d8061022de881d60fdb17036e5418ef632a7b
sayman <mdasadsayman92@gmail.com>
2026-04-15 20:22:43 +0600

    refactor API services to centralized configuration and fix POS filters

--- src/services/patient-service.ts
+++ src/services/patient-service.ts
@@ -11,31 +11,7 @@ import {
   DischargePayload,
   PharmacyPaymentPayload
 } from '@/types/patient';
-import axios from 'axios';
-import Cookies from 'js-cookie';
-
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://hms-srv.hamoodtech.com/api/v1';
-
-const api = axios.create({
-  baseURL: API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
-// Request interceptor to add the auth token header to every request
-api.interceptors.request.use(
-  (config) => {
-    const token = Cookies.get('accessToken');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+import { api } from '@/lib/api';
 
 export const patientService = {
   getPatients: async (params?: PatientQueryParams): Promise<PatientListResponse> => {
--- src/services/role-service.ts
+++ src/services/role-service.ts
@@ -5,39 +5,7 @@ import {
   RolePayload,
   RolesResponse
 } from '@/types/role';
-import axios from 'axios';
-import Cookies from 'js-cookie';
-
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://hms-srv.hamoodtech.com/api/v1';
-
-const api = axios.create({
-  baseURL: API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
-// Request interceptor to add the auth token header to every request
-api.interceptors.request.use(
-  (config) => {
-    const token = Cookies.get('accessToken');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-// Response interceptor to handle errors
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+import { api } from '@/lib/api';
 
 export const roleService = {
   getPermissions: async (): Promise<PermissionsResponse> => {
