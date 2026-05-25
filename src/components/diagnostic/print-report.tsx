4b3a321649e419ff7077893fafc4f62f844305fe
sayman <mdasadsayman92@gmail.com>
2026-05-25 18:52:41 +0600

    fix: remove 1.5in pt that was creating top gap before barcode

--- src/components/diagnostic/print-report.tsx
+++ src/components/diagnostic/print-report.tsx
@@ -171,7 +171,7 @@ export function PrintReport({ report }: PrintReportProps) {
                     style={{ width: "210mm", minHeight: "330mm", padding: "0" }}
                 >
 
-                    <div className="px-10 pt-[1.5in] pb-[0.6in] relative z-10 min-h-[13in] flex flex-col">
+                    <div className="px-10 pb-[0.6in] relative z-10 min-h-[13in] flex flex-col">
                         {/* Barcodes at Top Left and Top Right */}
                         <div className="flex justify-between items-start mb-6 px-1">
                             <div className="flex flex-col items-center">
